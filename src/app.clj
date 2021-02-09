b69f86d93a40aadb6868a5f16c9cb545763b9481
Nathan Moos <moosingin3space@gmail.com>
2021-02-08 21:54:00 -0800

    add more complete UI design

--- src/app.clj
+++ src/app.clj
@@ -3,34 +3,65 @@
   (:import [javafx.application Platform]))
 
 (def *state
-  (atom {:label-val "Hello world"
-         :showing true}))
+  (atom {:showing true}))
 
-(defn hello-world [{:keys [label-val]}]
+(defn header [{:keys [text]}]
   {:fx/type :label
-   :text label-val})
+   :text text
+   :padding {:top 10 :bottom 15 :left 0 :right 0}
+   :style {:-fx-font [:bold 24 :sans-serif]}})
 
-(defn text-input [{:keys [label-val]}]
-  {:fx/type :text-field
-   :text label-val
-   :on-text-changed #(swap! *state assoc :label-val %)})
+(defn upload-pane [& args]
+  {:fx/type :v-box
+   :children [{:fx/type header
+               :text "Upload"}
+              {:fx/type :h-box
+               :children [{:fx/type :text-field
+                           :prompt-text "File path"}
+                          {:fx/type :button
+                           :text "Browse"}]}
+              {:fx/type :button
+               :text "Upload!"}
+              {:fx/type :h-box
+               :children [{:fx/type :text-field
+                           :editable false
+                           :prompt-text "Generated code goes here"}
+                          {:fx/type :button
+                           :text "Copy"}]}]})
 
-(defn root [{:keys [showing label-val]}]
+(defn download-pane [& args]
+  {:fx/type :v-box
+   :children [{:fx/type header
+               :text "Download"}
+              {:fx/type :h-box
+               :children [{:fx/type :text-field
+                           :prompt-text "Wormhole code"}
+                          {:fx/type :button
+                           :text "Download"}]}
+              {:fx/type :h-box
+               :children [{:fx/type :text-field
+                           :prompt-text "Destination"}
+                          {:fx/type :button
+                           :text "Browse"}]}
+              {:fx/type :progress-bar}]})
+
+(defn root [& args]
   {:fx/type :stage
-   :showing showing
+   :showing true
    :title "Magic Wormhole"
    :scene {:fx/type :scene
            :root {:fx/type :v-box
-                  :padding 25
-                  :spacing 40
-                  :children [{:fx/type hello-world
-                              :label-val label-val}
-                             {:fx/type text-input
-                              :label-val label-val}
-                             {:fx/type :button
-                              :text "Close"
-                              :on-action (fn [_]
-                                           (swap! *state assoc :showing false))}]}}})
+                  :spacing 5
+                  :children [{:fx/type :label
+                              :text "Magic Wormhole"
+                              :padding {:top 5 :bottom 0 :left 5 :right 0}
+                              :style {:-fx-font [:bold 30 :sans-serif]}}
+                             {:fx/type :h-box
+                                    :alignment :center
+                                    :spacing 20
+                                    :padding {:top 10 :bottom 20 :left 20 :right 20}
+                                    :children [{:fx/type upload-pane}
+                                               {:fx/type download-pane}]}]}}})
 
 (def renderer
   (fx/create-renderer
